680811d49c1a97666c38a3d6c55fef2aecf2f743
arman-sh <rosko1245@gmail.com>
2019-10-12 11:38:28 +0300

    first task

--- hw0/shamenov/build.sbt
+++ hw0/shamenov/build.sbt
@@ -8,3 +8,5 @@ scalaVersion := "2.11.12"
 libraryDependencies += "org.apache.spark" %% "spark-core" % "2.4.0"
 // https://mvnrepository.com/artifact/org.apache.spark/spark-sql
 libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.4.0"
+// https://mvnrepository.com/artifact/org.apache.spark/spark-mllib
+libraryDependencies += "org.apache.spark" %% "spark-mllib" % "2.4.0" % "runtime"
--- hw0/shamenov/src/main/scala/Application.scala
+++ hw0/shamenov/src/main/scala/Application.scala
@@ -1,38 +1,49 @@
 import org.apache.spark.sql.{SparkSession, functions}
 import org.apache.spark.SparkContext
+import org.apache.spark.sql.types.{StructField, StructType, StringType}
+
 
 object Application extends App {
 
   val spark = SparkSession.builder.appName("Ukraine").config("spark.master", "local").getOrCreate()
-  val path = "./ua_reactions.csv"
-  val textFileDataFrame = spark.read.format("csv").option("header", "true").load(path) // header = true
 
+  val pathCSV = "./ua_reactions.csv"
+  val pathTxt = "./excluded.txt"
+
+  val csvDataFrame = spark.read.format("csv").option("header", "true").load(pathCSV) // header = true
 
   // exclude UAs
 
-  val excludedFileDataFrame = spark.read.format("txt") // load txt
+  val schema = new StructType().add(StructField("ua_excluded", StringType, true))
+
+  val excludedRdd = spark.read.text(pathTxt).rdd // load txt
+
+  val excludedFileDataFrame = spark.createDataFrame(excludedRdd, schema);
 
   // First task
 
-  textFileDataFrame.groupBy("ua") // group by ua
-                              .agg(functions.sum("is_click").alias("clicks"), functions.count("is_click").alias("shows")) //  sum clicks for each ua
-                              .where("clicks > 5") // where > 5 clicks
-                              .orderBy(functions.desc("clicks")) // sort by desc
-                              .withColumn("CTR", functions.col("clicks") / functions.col("shows"))
-                              .show(5) // show 5
+  val num = csvDataFrame.count()
+
+
+  csvDataFrame.join(excludedFileDataFrame, csvDataFrame("ua") === excludedFileDataFrame("ua_excluded"), "left_outer")
+       .filter("ua_excluded is null")
+
+  csvDataFrame.groupBy("ua") // group by ua
+    .agg(functions.sum("is_click").alias("clicks"), functions.count("is_click").alias("shows")) //  sum clicks for each ua
+    .where("clicks > 5")
+    .orderBy(functions.desc("clicks"))
+    .withColumn("CTR", functions.col("clicks") / functions.col("shows"))
+    .show(5) // show 5
+
+  val num1 = csvDataFrame.count()
 
   // Second task
 
-  
+  val total = csvDataFrame.agg(functions.sum("is_click")).first.getDouble(0).toInt //
 
-
-
-  val total = textFileDataFrame.agg(functions.sum("is_click")).first.getDouble(0).toInt //
-
-  val PercentsDF = textFileDataFrame.groupBy("ua") // group by ua
-            .agg(functions.sum("is_click").alias("clicks"))
-            .withColumn("percents", functions.col("clicks") / functions.sum("clicks").over()) // + new column
-
+  val PercentsDF = csvDataFrame.groupBy("ua") // group by ua
+    .agg(functions.sum("is_click").alias("clicks"))
+    .withColumn("percents", functions.col("clicks") / functions.sum("clicks").over())
   //println(PercentsDF.orderBy(functions.desc("percents")).limit(14).agg(functions.sum("percents")).first.getDouble(0))
 
 }
