439a20b284ed50867e37bedb9ad70619adcaf101
senyast4745 <senyasdt4745@gmail.com>
2019-10-15 02:42:24 +0300

    Fix Task

--- src/main/scala/com/github/senyast4745/firstML/MainClass.scala
+++ src/main/scala/com/github/senyast4745/firstML/MainClass.scala
@@ -26,10 +26,23 @@ object MainClass {
 //df.filter(r => !list.contains(r(0))).groupBy(df.columns.apply(0).select).show()
       // ...copy paste that for columns y and z
 
-    val as =  df.filter(r => r(1) == "1" && !list.contains(r(0))).groupBy(df.columns.apply(0))
-    .count()
-    as.sort(as.col("count").desc).show(5)
-    /*.where(functions.col('count') > 1)
+    val first = df.filter(r => r(1) == "1" && !list.contains(r(0)))
+    val as =  first.groupBy(df.columns.apply(0))
+      .count()
+    var c = 0L
+    as.filter(r => r.apply(1).asInstanceOf[Long] > 5).sort(as.col("count").desc).show(5)
+    val count = first.count() * 0.5;
+    as.sort(as.col("count").desc).filter(r => {
+      c += r(1).asInstanceOf[Long]
+      c < count
+    }).show()
+
+    println(count * 2)
+    println(c)
+
+
+//    as.sort(as.col("count").desc).ma
+    /*.where(functiongs.col('count') > 1)
     .select(functions.sum('count'))
     .show()*/
 //    print(answ.collect().length)
