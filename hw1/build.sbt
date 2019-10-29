e935dd60312e5b46067239ff36a7dd01787f933c
arman-sh <rosko1245@gmail.com>
2019-10-29 15:19:47 +0300

    labels and features

--- hw1/build.sbt
+++ hw1/build.sbt
@@ -9,4 +9,4 @@ libraryDependencies += "org.apache.spark" %% "spark-core" % "2.4.0"
 // https://mvnrepository.com/artifact/org.apache.spark/spark-sql
 libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.4.0"
 // https://mvnrepository.com/artifact/org.apache.spark/spark-mllib
-libraryDependencies += "org.apache.spark" %% "spark-mllib" % "2.4.0" % "runtime"
+libraryDependencies += "org.apache.spark" %% "spark-mllib" % "2.4.0"
--- hw1/src/main/scala/Classification.scala
+++ hw1/src/main/scala/Classification.scala
@@ -1,19 +1,40 @@
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.ml.Pipeline
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.ml.classification.{DecisionTreeClassifier, NaiveBayes}
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
+import org.apache.spark.ml.feature.{IndexToString, LabeledPoint}
+import org.apache.spark.ml.linalg.Vectors
 import org.apache.spark.mllib.tree.DecisionTree
-import org.apache.spark.mllib.tree.model.DecisionTreeModel
-import org.apache.spark.mllib.util.MLUtils
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
 
-object Classification extends App {
 
-      val spark = SparkSession.builder().appName("Classifier").config("spark.master", "local").getOrCreate()
+object Classification {
+
+    def main(args: Array[String]): Unit = {
+
 
       val pathDataSetCSV = "./ml_dataset.csv"
+      val spark = SparkSession.builder().appName("Classifier").config("spark.master", "local").getOrCreate()
+      var watherRaw: RDD[String] = spark.read.format("csv").option("header", "true").load(pathDataSetCSV).rdd.map(_.mkString(","))
 
-      var csvDataFrame = spark.read.format("csv").option("header", "true").load(pathDataSetCSV)
-      val featuresDataFrame = csvDataFrame.drop("label")
-      val labelDataFrame = csvDataFrame.select("label")
-      csvDataFrame = null
+      import spark.implicits._
 
-      spark.stop();
+      val dataRaw = watherRaw.map(_.split(",")).map({
+          csv => val label = csv.last.toDouble
+          val point = csv.init.map(_.toDouble)
+            (label, point)
+        }
+      )
+      val data: Dataset[LabeledPoint] = dataRaw
+        .map { case (label, point) =>
+          LabeledPoint(label, Vectors.dense(point))
+        }.toDS()
 
+      val Array(training: Dataset[LabeledPoint], test: Dataset[LabeledPoint]) =
+        data.randomSplit(Array(0.85, 0.15), seed = 1234L)
+      
+
+      spark.stop()
+    }
 }
