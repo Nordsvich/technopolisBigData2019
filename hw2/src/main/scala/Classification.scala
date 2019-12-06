32c964259997e3b18f5b4a381350d2f15d203135
arman-sh <rosko1245@gmail.com>
2019-12-06 16:34:30 +0300

    df

--- hw2/src/main/scala/Classification.scala
+++ hw2/src/main/scala/Classification.scala
@@ -7,41 +7,47 @@ object Classification {
 
   def main(args: Array[String]): Unit = {
 
-    val dataPath = "./mlboot_data.tsv"
-    val testPath = "./mlboot_test.tsv"
-    val trainPath = "./mlboot_train_answers.tsv"
-
+    val dataPath = "./mlboot_data.tsv" // 11 GB
+    val testPath = "./mlboot_test.tsv" // 6MB
+    val trainPath = "./mlboot_train_answers.tsv" // 15 MB
 
     val spark = SparkSession.builder().appName("Classifier")
       .config("spark.driver.maxResultSize", "2g")
       .config("spark.sql.broadcastTimeout", "36000")
       .config("spark.master", "local").getOrCreate()
 
+    import spark.implicits._
+    import org.apache.spark.sql.types._
+
+    val schema = StructType(Array(
+      StructField("cuid", StringType, nullable = true),
+      StructField("cat_feature", LongType, nullable = true),
+      StructField("feature_1", StringType, nullable = true),
+      StructField("feature_2", StringType, nullable = true),
+      StructField("feature_3", StringType, nullable = true),
+      StructField("dt_diff", LongType, nullable = true))
+    )
+
     val dataDF = spark.read.format("csv")
       .option("header", "false")
       .option("delimiter", "\t")
-      .load(dataPath)
-      .withColumnRenamed("_c0", "cuid")
-      .withColumnRenamed("_c1", "cat_feature")
-      .withColumnRenamed("_c2", "feature_1")
-      .withColumnRenamed("_c3", "feature_2")
-      .withColumnRenamed("_c4", "feature_3")
-      .withColumnRenamed("_c5", "dt_diff")
-      .repartition(2000)
+      .schema(schema)
+      .csv(spark.sparkContext.textFile(dataPath, 500).toDS())
 
+    val testDF = joinDf(testPath, spark, dataDF)
+    val trainDF = joinDf(trainPath, spark, dataDF)
 
+    spark.stop()
+  }
+
+  def joinDf(path: String,
+             spark: SparkSession,
+             dataDF: DataFrame): DataFrame = {
     val testDF = spark.read.format("csv")
       .option("header", "true")
       .option("delimiter", "\t")
-      .load(testPath)
-
-    val trainDF = spark.read.format("csv")
-      .option("header", "true")
-      .option("delimiter", "\t")
-      .load(trainPath)
-      .join(broadcast(dataDF),Seq("cuid"),"inner")
-      .show(6, truncate = false)
-
-    spark.stop()
+      .load(path)
+      .join(dataDF, Seq("cuid"), "inner")
+    testDF;
   }
 }
